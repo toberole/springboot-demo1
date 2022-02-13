5928b2d6a30d4277ceafa09f8943ee0d3b02599d
zhouwei441300 <zhouwei441300@163.com>
2022-02-13 15:50:51 +0800

    first

---
+++ src/main/java/com/example/test/demo/UserController.java
@@ -0,0 +1,21 @@
+package com.example.test.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+// 访问 http://127.0.0.1:8080/springboot/getUserByGet?userName=hello
+//@RestController注解能够使项目支持Rest
+@RestController
+@SpringBootApplication
+//表示该controller类下所有的方法都公用的一级上下文根
+@RequestMapping(value = "/springboot")
+public class UserController {
+    //这里使用@RequestMapping注解表示该方法对应的二级上下文路径
+    @RequestMapping(value = "/getUserByGet", method = RequestMethod.GET)
+    String getUserByGet(@RequestParam(value = "userName") String userName){
+        return "Hello " + userName;
+    }
+}
